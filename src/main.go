a16d2cf60329543a9b0dd03aafdbf398b951f348
mauri1996 <mauri1996@hotmail.es>
2022-04-16 14:58:32 -0500

    Creacion de funciones

--- src/main.go
+++ src/main.go
@@ -2,32 +2,15 @@ package main
 
 import "fmt"
 
+func saludar(nombre string) {
+	fmt.Println("Hola", nombre)
+}
+
+func sumar(a, b float64) float64 {
+	return a + b
+}
+
 func main() {
-
-	// doc https://pkg.go.dev/fmt
-
-	helloMessage := "Hello"
-	worldMessage := "world"
-
-	// Println -> imprime en 1 linea
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
-
-	//Printf
-	nombre := "Platzi"
-	cursos := 500
-	// %s string
-	// %d int
-	// %v si no se sabe los tipos de datos
-	fmt.Printf("%s tiene mas de %d cursos \n", nombre, cursos) // Platzi tiene mas de 500 cursos
-	fmt.Printf("%v tiene mas de %v cursos \n", nombre, cursos) // Platzi tiene mas de 500 cursos
-
-	//Sprintf
-	// todo lo generado se devuelve a message
-	message := fmt.Sprintf("%s tiene mas de %d cursos", nombre, cursos)
-	fmt.Println(message)
-
-	//Tipo de dato
-	fmt.Printf("helloMessage: %T \n", helloMessage) //helloMessage: string
-	fmt.Printf("Cursos: %T\n", cursos)              // Cursos: int
+	saludar("Mauri")
+	fmt.Println("Suma de 10 y 20:", sumar(10, 20))
 }
