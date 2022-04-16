73e81d08604c42f1acc152ab18fc75955e5d8514
mauri1996 <mauri1996@hotmail.es>
2022-04-16 13:21:06 -0500

    Suma, resta, multiplicacion, division, modulo y algunas areas

--- src/main.go
+++ src/main.go
@@ -3,33 +3,66 @@ package main
 import "fmt"
 
 func main() {
-	// Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de variables enteras
-	base := 12 // : dice que la variable no fue creada anteriormente
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero Value -> valor por defecto
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d) // 0 0   false
 
 	// Area de un cuadrado
-
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area Cuadrado:", areaCuadrado)
 
-	fmt.Println("Area Cuadrado: ", areaCuadrado)
+	x := 10
+	y := 50
 
+	//Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	//Resta
+	result = y - x // se coloca = puesto uqe ya fue ini
+	fmt.Println("Resta:", result)
+
+	//Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	//Division
+	result = y / x
+	fmt.Println("Division:", result)
+
+	//Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+
+	//Incremental
+	x++
+	fmt.Println("Incremental: ", x)
+
+	//Decremental
+	x--
+	fmt.Println("Decremental: ", x)
+
+	// Reto Calculo de area de:
+	// Rectangulo, Trapecio y Circulo
+
+	// Variables
+	x = 2
+	y = 3
+
+	// Rectangulo
+	AreaRectangulo := x * y
+	fmt.Println("Area de rectangulo:", AreaRectangulo)
+
+	// Trapecio
+	const a float64 = 2
+	const b float64 = 3
+	const h float64 = 3
+
+	AreaTrapecio := h * ((a + b) / 2)
+	fmt.Println("Area de trapecio:", AreaTrapecio)
+
+	// Circulo
+	const pi float64 = 3.1415
+	const radio = 2
+
+	AreaCirculo := radio * pi
+	fmt.Println("Area de circulo:", AreaCirculo)
 }
